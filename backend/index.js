ec66fa28d22259960155fcd1d35e8a1cf57d5967
sharad1666 <sharad.dac@gmail.com>
2025-11-16 00:50:50 +0530

    Update index.js

--- backend/index.js
+++ backend/index.js
@@ -13,7 +13,7 @@ import geminiResponse from "./gemini.js"
 const app=express()
 
 app.use(cors({
-    origin:"http://localhost:5173",
+    origin:"https://virtual-assistant-3e6k.onrender.com",
     credentials:true
 }))
 const port=process.env.PORT || 5000
